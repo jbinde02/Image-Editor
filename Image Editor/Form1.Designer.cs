a58708c4eec956c6ce472bc1d7c4f66a8c4e05f2
jbinde02 <jbinde02@rams.shepherd.edu>
2020-11-17 17:38:22 -0500

    Changed Numerous Things: Changed Image types to Bitmaps Added numerous controls Cleaned up the code

--- Image Editor/Form1.Designer.cs
+++ Image Editor/Form1.Designer.cs
@@ -41,8 +41,13 @@
             this.cropToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.paintToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.filterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resizeToolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.defaultWindowSizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.fitToImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -54,13 +59,13 @@
             this.pictureBox1.Size = new System.Drawing.Size(901, 484);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
-            this.toolsToolStripMenuItem});
+            this.toolsToolStripMenuItem,
+            this.toolStripMenuItem1});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(924, 24);
@@ -83,39 +88,41 @@
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.openToolStripMenuItem.Text = "Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
             // openRecentToolStripMenuItem
             // 
             this.openRecentToolStripMenuItem.Name = "openRecentToolStripMenuItem";
-            this.openRecentToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.openRecentToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.openRecentToolStripMenuItem.Text = "Open Recent";
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
             // saveAsToolStripMenuItem
             // 
             this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
-            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
             // 
             // closeToolStripMenuItem
             // 
             this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
-            this.closeToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.closeToolStripMenuItem.Text = "Close";
             this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
@@ -124,7 +131,8 @@
             this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.cropToolStripMenuItem,
             this.paintToolStripMenuItem,
-            this.filterToolStripMenuItem});
+            this.filterToolStripMenuItem,
+            this.resizeToolStripMenuItem});
             this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
             this.toolsToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
             this.toolsToolStripMenuItem.Text = "Tools";
@@ -132,24 +140,64 @@
             // cropToolStripMenuItem
             // 
             this.cropToolStripMenuItem.Name = "cropToolStripMenuItem";
-            this.cropToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.cropToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.cropToolStripMenuItem.Text = "Crop";
+            this.cropToolStripMenuItem.Click += new System.EventHandler(this.cropToolStripMenuItem_Click);
             // 
             // paintToolStripMenuItem
             // 
             this.paintToolStripMenuItem.Name = "paintToolStripMenuItem";
-            this.paintToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.paintToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
             this.paintToolStripMenuItem.Text = "Paint";
             // 
             // filterToolStripMenuItem
             // 
             this.filterToolStripMenuItem.Name = "filterToolStripMenuItem";
-            this.filterToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.filterToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
             this.filterToolStripMenuItem.Text = "Filter";
+            // 
+            // resizeToolStripMenuItem
+            // 
+            this.resizeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resizeToolStripTextBox1});
+            this.resizeToolStripMenuItem.Name = "resizeToolStripMenuItem";
+            this.resizeToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.resizeToolStripMenuItem.Text = "Resize by %";
+            // 
+            // resizeToolStripTextBox1
+            // 
+            this.resizeToolStripTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.resizeToolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.resizeToolStripTextBox1.Name = "resizeToolStripTextBox1";
+            this.resizeToolStripTextBox1.Size = new System.Drawing.Size(100, 23);
+            this.resizeToolStripTextBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.resizeToolStripTextBox1_KeyUp);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.defaultWindowSizeToolStripMenuItem,
+            this.fitToImageToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(44, 20);
+            this.toolStripMenuItem1.Text = "View";
+            // 
+            // defaultWindowSizeToolStripMenuItem
+            // 
+            this.defaultWindowSizeToolStripMenuItem.Name = "defaultWindowSizeToolStripMenuItem";
+            this.defaultWindowSizeToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.defaultWindowSizeToolStripMenuItem.Text = "Default Window Size";
+            this.defaultWindowSizeToolStripMenuItem.Click += new System.EventHandler(this.defaultWindowSizeToolStripMenuItem_Click);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // fitToImageToolStripMenuItem
+            // 
+            this.fitToImageToolStripMenuItem.Name = "fitToImageToolStripMenuItem";
+            this.fitToImageToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.fitToImageToolStripMenuItem.Text = "Fit to Image";
+            this.fitToImageToolStripMenuItem.Click += new System.EventHandler(this.fitToImageToolStripMenuItem_Click);
             // 
             // Form1
             // 
@@ -186,6 +234,11 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem defaultWindowSizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox resizeToolStripTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem fitToImageToolStripMenuItem;
     }
 }
 
--- Image Editor/Form1.cs
+++ Image Editor/Form1.cs
@@ -12,42 +12,151 @@ namespace Image_Editor
 {
     public partial class Form1 : Form
     {
+        private Bitmap originalImg = new Bitmap(1,1);
+        private Bitmap img = new Bitmap(1,1);
+        private string path;
+        private Size defaultWindowSize = new Size(940, 560);
 
-        Image img;
         public Form1()
         {
             InitializeComponent();
         }
 
+        //File menubar start
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
             if(openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                img = Image.FromFile(openFileDialog1.FileName);
-                pictureBox1.Size = img.Size;
-                if(pictureBox1.Size.Width > this.Size.Width || pictureBox1.Size.Height > this.Size.Height)
+                try
                 {
-                    this.Size = pictureBox1.Size;
+                    originalImg = new Bitmap(openFileDialog1.FileName);
+                    img = new Bitmap(openFileDialog1.FileName);
                 }
+                catch(Exception exception)
+                {
+                    MessageBox.Show("Invalid File Type\n" + exception.Message);
+                    return;
+                }
+                originalImg.Dispose();
+                path = openFileDialog1.FileName;
+                resizePictureBox();
                 pictureBox1.Image = img;
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Bitmap imgToSave = new Bitmap(img);
+            img.Dispose();
+            try
+            {
+                //Since file exist, delete and then resave
+                if (System.IO.File.Exists(path))
+                {
+                    System.IO.File.Delete(path);
+                }
+                imgToSave.Save(path, System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+            img = new Bitmap(imgToSave);
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.DefaultExt = "jpg";
+            saveFileDialog1.FileName = System.IO.Path.GetFileName(path);
+            Bitmap imgToSave = new Bitmap(img);
+            img.Dispose();
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    //If file exist, delete and then resave
+                    if (System.IO.File.Exists(path))
+                    {
+                        System.IO.File.Delete(path);
+                    }
+                    imgToSave.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    path = saveFileDialog1.FileName;
+                }
+                catch(Exception exception)
+                {
+                    MessageBox.Show(exception.Message);
+                }
+                img = new Bitmap(imgToSave);
             }
         }
 
         private void closeToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            originalImg = null;
             img = null;
+            path = null;
             pictureBox1.Image = img;
-            this.Size = new Size(940, 560);
+            this.Size = defaultWindowSize;
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+        //File menubar end
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        //Tools menubar start
+        private void cropToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            
+        }
 
+        private void resizeToolStripTextBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                if(double.TryParse(resizeToolStripTextBox1.Text, out double ratio))
+                {
+                    ratio = ratio / 100;
+                    img = new Bitmap(img, new Size((int)(img.Width * ratio), (int)(img.Height * ratio)));
+                    refresh();
+                    resizeToolStripTextBox1.Text = "";
+                }
+            }
+        }
+        //Tools menubar end
+
+        //View menubar start
+        private void defaultWindowSizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Size = defaultWindowSize;
+        }
+
+        private void fitToImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Size = img.Size;
+        }
+        //View menubar end
+
+        private void resizePictureBox()
+        {
+            pictureBox1.Size = img.Size;
+            if (pictureBox1.Size.Width > this.Size.Width || pictureBox1.Size.Height > this.Size.Height)
+            {
+                this.Size = pictureBox1.Size;
+            }
+        }
+
+        private void refreshImage()
+        {
+            pictureBox1.Image = img;
+        }
+
+        //Call this whenever changes are made to the image
+        private void refresh()
+        {
+            resizePictureBox();
+            refreshImage();
         }
     }
 }
